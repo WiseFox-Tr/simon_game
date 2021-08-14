cdfe328777558693b1370edc53dd9832a70287cc
Maxime Traverse <maxime.Traverse@orange.fr>
2021-08-14 19:34:06 +0200

    create a function to manage game over case + create a function to check if user answer is good or not for each sequence & determine the next step of game + refactor comments

--- index.js
+++ index.js
@@ -1,41 +1,43 @@
 //create a new pattern
 let buttonColors = ["red", "blue", "green", "yellow"]; 
+let isNotStarted = true; 
 let gamePattern = []; 
 let userClickedPattern = []; 
-let isNotStarted = true; 
 let level = 0; 
+let sequenceChecked = 0; 
 
 //each time player hits keyboard
 $("body").keydown(function() {
     if(isNotStarted) {
-        isNotStarted = !isNotStarted; 
+        isNotStarted = false; 
         nextSequence();
     }
 });
 
-//detect user clicks, store color of triggered button & play corresponding sound + animate it
+//store color of triggered button, play corresponding sound & animate it
+//also call check answer function to see if user succeed or failed and update the game state
 $(".btn").click(function() {
     let userChosenColor = $(this).attr("id"); 
     userClickedPattern.push(userChosenColor); 
     animatePressedButton(userChosenColor);
     playSound(userChosenColor);
+
+    //check user's answer and determine what to do next 
+    checkAnswerAndUpdateGameState(level); 
 });
 
 /**
- * 1- update level and display it
- * 
- * 2- generate a random number between 0 & 3 & push color into game pattern
- * 
- * 3 - show sequence to user (flash animation + play sound)
+ * + update level and display it
+ * + generate a random number between 0 & 3 & push corresponding color into game pattern array
+ * + show next sequence to user (flash animation + play sound)
  */
 function nextSequence() {
-    //1
     level++;
     $("h1").text("Level " + level);
-    //2
+
     let randomNumber = Math.floor(Math.random() * 4); 
     gamePattern.push(buttonColors[randomNumber]); 
-    //3
+
     let sequenceButton = $("#" + gamePattern[gamePattern.length -1]);
     sequenceButton.fadeOut(100).fadeIn(100);
     playSound(sequenceButton.attr("id"));
@@ -56,11 +58,33 @@ function nextSequence() {
 }
 
 /**
+ * Control user answer and choose the next stage depending on :
+ * + the thuth (or not) of user answer
+ * + the current sequence checked
+ * @param {int} currentLevel
+ */
+function checkAnswerAndUpdateGameState(currentLevel) { 
+    //if user answer is different to game pattern for the sequence checked -> game over, else -> update sequenceChecked to control the next sequence   
+    gamePattern[sequenceChecked] === userClickedPattern[sequenceChecked] ? sequenceChecked++ : displayGameOver(); 
+
+    //if all user click's pattern is valid -> start new sequence + reset control values
+    if(sequenceChecked === currentLevel) {
+        setTimeout(
+            function() {
+                nextSequence();
+                userClickedPattern.length = 0; 
+                sequenceChecked = 0; 
+            },
+            1000
+        );
+    }
+}
+
+/**
  * play sound corresponding to idSound param
  * @param {*string} idSound 
  */
 function playSound(idSound) {
-    console.log("id value --> " + idSound);
     switch(idSound) {
         case "blue" : new Audio("sounds/blue.mp3").play(); break;
         case "green" : new Audio("sounds/green.mp3").play(); break;    
@@ -69,4 +93,20 @@ function playSound(idSound) {
         case "wrong" : new Audio("sounds/wrong.mp3").play(); break;      
         default : console.log("Error : idSound received = " + idSound); 
     }
+}
+
+/**
+ * play wrong sound and launch gameOver animation
+ * also update title 
+ */
+function displayGameOver() {
+    playSound("wrong");
+    $("body").addClass("game-over");
+    setTimeout(
+        function() {
+            $("body").removeClass("game-over");
+        },
+        100
+    );
+    $("h1").text("Game over, Press any key to restart the game")
 }
